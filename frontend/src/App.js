b64a50db37af7e22a7f8091e47f91e40591667aa
kylewgunter <kylewgunter@gmail.com>
2021-08-03 16:10:24 -0500

    Kyle/Shawn wip

---
+++ frontend/src/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import { getLoggedInUser, login } from './api/UserAPI';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      if (localStorage.getItem("auth-user") !== 'null') {
+        let response = await getLoggedInUser(localStorage.getItem("auth-user"));
+        let data = await response.json();
+        if (data.username) {
+          setIsLoggedIn(true);
+          setUser(data);
+        }
+      }
+    }
+    if (!user) {
+      getUser();
+    }
+  }, [user])
+
+  const handleLogin = async (evt) => {
+    evt.preventDefault();
+    let userObject = {
+      username: evt.target.username.value,
+      password: evt.target.password.value,
+    }
+    let response = await login(userObject);
+    let data = await response.json();
+    if (data.token) {
+      localStorage.setItem("auth-user", `${data.token}`);
+      setIsLoggedIn(true);
+      setUser(data.user);
+    }
+  }
+
+  const handleLogout = () => {
+    localStorage.setItem("auth-user", null);
+    setIsLoggedIn(false);
+    setUser(null);
+  }
+
+  const renderLoginPage = () => {
+    return (
+      <LoginPage
+        isLoggedIn={isLoggedIn}
+        handleLogin={handleLogin}
+        handleLogout={handleLogout}
+        user={user}
+      />
+    )
+  }
+
+  const renderHomePage = () => {
+    return (
+      <HomePage
+        isLoggedIn={isLoggedIn}
+        user={user}
+        handleLogout={handleLogout}
+      />
+    )
+  }
+
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Route exact path="/" render={renderHomePage} />
+          <Route exact path="/login" render={renderLoginPage} />
+          <Route exact path="/signup" component={SignupPage} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
